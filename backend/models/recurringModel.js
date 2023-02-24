6ec146f0f18feb9d283cd0fbc3d3ba71771bf37e
Zeel <zeeldesai2163@gmail.com>
2023-02-24 12:52:03 +0530

    Added models of purchase and recurring files

--- backend/models/recurringModel.js
+++ backend/models/recurringModel.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const recurring = mongoose.Schema({
-  Sr_No: String,
-  Description_of_Material: String,
-  Name_Of_Supplier: String,
-  Bill_No: String,
-  Date: String,
-  Amount: String,
-  Material: String,
-  Receiving_Year: String,
-  Year: String,
-  Yearly_expense: String,
-  Department: String,
+  Sr_No: { type: String, default: "" },
+  Description_of_Material: { type: String, default: "" },
+  Name_Of_Supplier: { type: String, default: "" },
+  Bill_No: { type: String, default: "" },
+  Date: { type: String, default: "" },
+  Amount: { type: String, default: "" },
+  Material: { type: String, default: "" },
+  Receiving_Year: { type: String, default: "" },
+  Year: { type: String, default: "" },
+  Yearly_expense: { type: String, default: "" },
+  Department: { type: String, default: "" },
 });
 
 module.exports = mongoose.model("Recurring", recurring);
